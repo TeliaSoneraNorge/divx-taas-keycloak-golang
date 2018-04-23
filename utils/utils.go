96e751f7fb58db5e3e5922eeb27774b5c447335b
Christopher Williams <christopher.williams@telia.no>
2018-04-23 21:32:35 +0200

    Adding ExtractTokenFromBearerHeader

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,8 @@
 package utils
 
+import "strings"
+
+// Is the string in a simple array
 func InArray(arr []string, str string) bool {
 	for _, a := range arr {
 		if a == str {
@@ -7,4 +10,10 @@ func InArray(arr []string, str string) bool {
 		}
 	}
 	return false
+}
+
+// Extract the string after the Bearer
+func ExtractTokenFromBearerHeader(raw string) string {
+	accessToken := strings.SplitAfter(raw, "Bearer ")[1]
+	return accessToken
 }
